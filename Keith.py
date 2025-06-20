3e26603680f0b3d5664407a9be44fc4026cb5f98
Jay362880 <jaystiller6@gmail.com>
2025-06-21 02:36:41 +1000

    Add files via upload

---
+++ Keith.py
@@ -0,0 +1,28 @@
+occupation = input("enter an occupation: ")
+name = input("enter a name: ")
+
+if occupation[0] == "a":
+    a = "an"
+
+elif occupation[0] == "e":
+    a = "an"
+
+elif occupation[0] == "i":
+    a = "an"
+
+elif occupation[0] == "o":
+    a = "an"
+
+elif occupation[0] == "u":
+    a = "an"
+
+else:
+    a = "a"
+
+print("There once was " + a + " " + occupation + " named " + name)
+print("Who circumcised men with his teeth")
+print("Twas not for leisure or sexual pleasure")
+print("But to get at the cheese underneath")
+print(" ")
+
+input("press enter to close")
